3f6a7022cc0c822bb85da53d050061df84122a60
Shawn Evans <shawnevans328@gmail.com>
2025-11-18 20:17:23 -0500

    Now centers properly.

--- timesheet.py
+++ timesheet.py
@@ -1,17 +1,36 @@
-from pypdf import PdfReader, PdfWriter # type: ignore
 
-reader = PdfReader("timesheet.pdf")
+from pypdf import PdfReader, PdfWriter # type: ignore
+import os
+
+reader = PdfReader("init.pdf")
 writer = PdfWriter()
 
-page = reader.pages[0]
-fields = reader.get_fields()
-
 writer.append(reader)
+writer.set_need_appearances_writer(True)
+page = writer.pages[0]
+fields = writer.get_fields()
 
-writer.update_page_form_field_values(
-    writer.pages[0],
-    {"topmostSubform[0].Page1[0].TextField1[10]": "dogs"},
-    auto_regenerate=False,
-)
+writer.update_page_form_field_values(page,{f"topmostSubform[0].Page1[0].TextField1[7]": "Shawn Evans"},auto_regenerate=True,)
+writer.update_page_form_field_values(page,{f"topmostSubform[0].Page1[0].TextField1[84]": "11/28"},auto_regenerate=True,)
+# writer.update_page_form_field_values(page, fields, flatten=True)
+# writer.remove_annotations(subtypes="/Widget")
 
-writer.write("out-filled-form.pdf")
+writer.write("timesheet.pdf")
+
+os.startfile("timesheet.pdf")
+
+# sun = 10
+# mon = 14
+# tues = 18
+# wed = 22
+# thurs = 26
+# fri = 30
+# sat = 34
+
+# sun = 75
+# mon = 84
+# tues = 85
+# wed = 86
+# thurs = 87
+# fri = 88
+# sat = 89
