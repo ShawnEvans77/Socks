fae796528282b31faa4646a58be992aacf021f8f
Shawn Evans <shawnevans328@gmail.com>
2025-11-19 14:52:39 -0500

    VERSION: Alpha 1.0, capable of writing to PDF using a CLI.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,8 @@
-
-import os
 import sheet_writer as sw
 from pypdf import PdfWriter # type: ignore
 
 def main():
-    writer = PdfWriter("resources/init.pdf")
-    page = writer.pages[0]
-
-    sheet = sw.SheetWriter(writer, page)
+    sheet = sw.SheetWriter("resources/init.pdf")
 
     print("************************************************")
     last = input("Please enter your last name: ")
@@ -20,8 +15,7 @@ def main():
     sheet.write_pay_period(pay_period)
     print("************************************************")
 
-    writer.write("timesheet.pdf")
-    os.startfile("timesheet.pdf")
+    sheet.output("timesheet.pdf")
 
 if __name__ == '__main__':
     main()
--- src/sheet_writer.py
+++ src/sheet_writer.py
@@ -1,15 +1,16 @@
 from pypdf import PdfReader, PdfWriter # type: ignore
 import pay_table as pt
 import datetime as d
+import os
 
 class SheetWriter:
 
     last_pos, first_pos = 6, 7 # SIX SEVEN!!! :3
     pay_pos = 5
 
-    def __init__(self, writer, page):
-        self.writer = writer
-        self.page = page
+    def __init__(self, input_file):
+        self.writer = PdfWriter(input_file)
+        self.page = self.writer.pages[0]
 
     def write_last(self, last_name):
         self.writer.update_page_form_field_values(self.page,{f"topmostSubform[0].Page1[0].TextField1[{SheetWriter.last_pos}]": last_name})
@@ -25,3 +26,9 @@ class SheetWriter:
 
         for day, position in enumerate(days):
             self.writer.update_page_form_field_values(self.page, {f"topmostSubform[0].Page1[0].TextField1[{position}]":  pay_table.offset_string(pay_period, day)} )
+
+    def output(self, output_file):
+        self.writer.write(output_file)
+        os.startfile("timesheet.pdf")
+
+
