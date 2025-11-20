1f1b9812b21688b8d372f07e7b16713c35ec3908
Shawn Evans <shawnevans328@gmail.com>
2025-11-19 19:50:45 -0500

    aa

--- src/pay_table.py
+++ src/pay_table.py
@@ -7,21 +7,21 @@ class PayTable:
         with open("resources/text input/pay_period.txt", "r") as file:
             for line in file:
                 tokens = line.split(',')
-                pay_period = tokens[0]
+                pay_period = int(tokens[0])
                 date = tokens[1].split('/')
                 self.pay_dict[pay_period] = d.datetime(month=int(date[0]), day=int(date[1]), year=int(date[2]))
 
-    def get(self, pay_period):
+    def get(self, pay_period: int):
         return self.pay_dict[pay_period]
     
-    def month_day(self, date) -> str:
+    def month_day(self, date: d.datetime) -> str:
         return date.strftime("%m/%d")
     
-    def start_date_string(self, pay_period) -> str:
+    def start_date_string(self, pay_period: int) -> str:
         return self.month_day(self.pay_dict[pay_period])
    
-    def end_date_string(self, pay_period) -> str:
+    def end_date_string(self, pay_period: int) -> str:
         return self.month_day(self.pay_dict[pay_period] + d.timedelta(days=13))
     
-    def date_offset_string(self, pay_period, offset) -> str:
+    def date_offset_string(self, pay_period: int, offset: int) -> str:
         return self.month_day(self.pay_dict[pay_period] + d.timedelta(days=offset))
