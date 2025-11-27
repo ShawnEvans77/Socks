4ca2e6f6021dea8ce1086e12453975e22b362012
Shawn Evans <shawnevans328@gmail.com>
2025-11-27 15:14:38 -0500

    reorganizing sheetwriter class vars to be more readable

--- src/sheet_writer.py
+++ src/sheet_writer.py
@@ -5,6 +5,9 @@ import schedule_table as st
 import clock as c
 
 class SheetWriter:
+    pay_table = pt.PayTable()
+    schedule_table = st.ScheduleTable()
+
     last_name_index, first_name_index = 6, 7 # SIX SEVEN!!! :3
     pay_period_index = 5
 
@@ -12,7 +15,12 @@ class SheetWriter:
     total_hours_index = (138, 141)
 
     week_one_date_indices = (10, 14, 18, 22, 26, 30, 34)
-    week_one_time_in_out_indices = ((11, 13), (15,17), (19,21), (23, 25), (27, 29), (31, 33), (35, 37))
+    week_two_date_indices = (75, 84, 85, 86, 87, 88, 89)
+    date_indices = week_one_date_indices + week_two_date_indices
+
+    week_one_time_in_out_indices = ((11, 13), (15,  17), (19,21),   (23, 25),  (27, 29),  (31, 33), (35, 37))
+    week_two_time_in_out_indices = ((76, 78), (90, 102), (91, 103), (92, 104), (93, 105), (94, 106),  (95, 107))
+    time_in_out_indices = week_one_time_in_out_indices + week_two_time_in_out_indices
 
     week_one_total_indices = (59, 60, 61, 62, 63, 64, 65)
     week_two_total_indices = (82, 126, 127, 128, 129, 130, 131)
@@ -20,17 +28,7 @@ class SheetWriter:
 
     week_one_hours_worked = (45, 46, 47, 48, 49, 50, 51)
     week_two_hours_worked = (79, 108, 109, 110, 111, 112, 113)
-
-    week_two_date_indices = (75, 84, 85, 86, 87, 88, 89)
-    week_two_time_in_out_indices = ((76, 78), (90, 102), (91, 103), (92, 104), (93, 105), (94, 106), (95, 107))
-
-    time_in_out_indices = week_one_time_in_out_indices + week_two_time_in_out_indices
-    date_indices = week_one_date_indices + week_two_date_indices
-    
     hours_worked_indices = week_one_hours_worked + week_two_hours_worked
-
-    pay_table = pt.PayTable()
-    schedule_table = st.ScheduleTable()
     
     def __init__(self, input_file_name: str, last_name: str, first_name: str, pay_period: int):
         self.writer = PdfWriter(input_file_name)
