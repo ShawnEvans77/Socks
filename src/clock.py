d76cc21a7bcc94f892280acfe386322cf97bff37
Shawn Evans <shawnevans328@gmail.com>
2026-02-10 20:43:42 -0500

    readability

--- src/clock.py
+++ src/clock.py
@@ -7,14 +7,11 @@ class Clock:
     """
 
     def __init__(self, time_str: str):
-
         hour_int = int(time_str[:-2])
         
         match time_str[-2:]:
-            case "PM":
-                self.hour = hour_int if hour_int == 12 else hour_int + 12
-            case "AM":
-                self.hour = 0 if hour_int == 12 else hour_int
+            case "PM": self.hour = hour_int if hour_int == 12 else hour_int + 12
+            case "AM": self.hour = 0 if hour_int == 12 else hour_int
 
     def __sub__(self, other) -> int:
         return other.hour - self.hour
